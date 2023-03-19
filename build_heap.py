7ff3728d21b5d5476fb595e7a0cacef38f06ee3f
DanielsTumans <125546247+DanielsTumans@users.noreply.github.com>
2023-03-19 13:41:16 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,62 +1,63 @@
-# python3
-
-
+#phyton3
 def build_heap(data):
     swaps = []
     n = len(data)
-    for i in range(n// 2 -1, -1, -1):
-   
+    for i in range (n// 2 -1 -1 -1):
       while True:
-        tn=i
-        l=2*i+1
-        r=2*i+2
- 
-        if l< n and data[l]< data[tn]:
-          tn= l
-        if r< n and data[r]< data[tn]:
-          tn= r
-        if i!= tn:
-          swaps.append((i, tn))
-          data[i], data[tn]= data[tn], data[i]
-          i=tn
+         bebr = i
+         left = 2 * i + 1
+         right = 2 * i + 2
+         if left < n and data[left] < data[bebr]:
+            bebr = left
+         if right < n and data[right] < data[bebr]:
+            bebr = right
+         if i!=bebr:
+            swaps.append((i, bebr))
+            data[i], data[bebr] = data[bebr], data[i]
+            i = bebr       
+         else:
+            break
+    return swaps        
+            
+            
+            
+            
+            
 
-        else:
-          break
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-    return swaps    
+
+
+    
+
+
 def main():
-    inp_type=input()
-    # TODO : add input and corresponding checks
-    # add another input for I or F
-    # first two tests are from keyboard, third test is from a file
-    if 'I' in inp_type:
-        # input from keyboard
+    bebroni = input()
+    if "I" in bebroni:
         n = int(input())
         data = list(map(int, input().split()))
         
-    elif 'F' in inp_type:
-        fn= input()
-        with open(f"tests/{fn}") as f:
+    elif "F" in bebroni:
+        files = input()
+        
+        with open(f"tests/{files}") as f:
             n = int(f.readline())
             data = list(map(int, f.readline().split()))
+    else:
+       return
+
     
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
-
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
+   
+    
 
-    # TODO: output how many swaps were made,
-    # this number should be less than 4n (less than 4*len(data))
+   
 
 
-    # output all swaps
+    
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
 
 
 if __name__ == "__main__":
-    main()
+   main()
