dac051ba566dcd5feb7370ccaeaebfd30b4469d7
DanielsTumans <125546247+DanielsTumans@users.noreply.github.com>
2023-03-19 13:25:22 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,25 +1,27 @@
 # python3
 
-def ssp(i, data, swaps):
-    n = len(data)
-    bebr = i
-    left = 2 * n + 1
-    right = 2 * n + 2
-    if left < n and data[bebr] > data[left]:
-        bebr = left
-    if right < n and data[bebr] < data[right]:
-        bebr = right
-    if i!=bebr:
-        swaps.append((i, bebr))
-        data[i], data[bebr] = data[bebr], data[i]
-        i = bebr
+
+   
    
 def build_heap(data):
     swaps = []
-   
+    n = len(data)
     for i in range (n// 2 -1 -1 -1):
-        swap(i, data, swaps)
-    return swaps        
+        while True:
+            bebr = i
+            left = 2 * i + 1
+            right = 2 * i + 2
+            if left < n and data[bebr] > data[left]:
+                bebr = left
+            if right < n and data[bebr] < data[right]:
+                bebr = right
+            if i!=bebr:
+                swaps.append((i, bebr))
+                data[i], data[bebr] = data[bebr], data[i]
+                i = bebr       
+            else:
+                break
+        return swaps        
             
             
             
