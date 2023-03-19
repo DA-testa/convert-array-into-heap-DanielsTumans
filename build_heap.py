8365280c05684621344a83f936c06c7a46bb06e6
DanielsTumans <125546247+DanielsTumans@users.noreply.github.com>
2023-03-19 13:20:20 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -1,31 +1,35 @@
 # python3
 
-
+def ssp(i, data, swaps):
+    n = len(data)
+    bebr = i
+    left = 2 * n + 1
+    right = 2 * n + 2
+    if left < n and data[bebr] > data[left]:
+        bebr = left
+    if right < n and data[bebr] < data[right]:
+        bebr = right
+    if i!=bebr:
+        swaps.append((i, bebr))
+        data[i], data[bebr] = data[bebr], data[i]
+        i = bebr
+   
 def build_heap(data):
     swaps = []
-    n = len(data)
+   
     for i in range (n// 2 -1 -1 -1):
-        while True: 
-            bebr = i
-            left = 2 * n + 1
-            right = 2 * n + 2
+        swap(i, data, swaps)
+    return swaps        
             
             
             
-            if left < n and data[bebr] > data[left]:
-                bebr = left
-            if right < n and data[bebr] < data[right]:
-                bebr = right
-            if i!=bebr:
-                swaps.append((i, bebr))
-                data[i], data[bebr] = data[bebr], data[i]
-                i = bebr
+            
             
     # TODO: Creat heap and heap sort
     # try to achieve  O(n) and not O(n2)
 
 
-    return swaps
+    
 
 
 def main():
